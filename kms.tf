a67b2d054432e1f1fceaba530cbe110a6ecbe99a
Cristian Beraha <cristian@cloudops.works>
2024-12-14 12:10:00 -0300

    chore: FIX kms key added Cloudwatch statement

--- kms.tf
+++ kms.tf
@@ -6,7 +6,7 @@
 
 module "kms" {
   count      = try(var.kms.create, false) ? 1 : 0
-  source     = "github.com/cloudopsworks/terraform-module-aws-kms.git?ref=v1.0.0"
+  source     = "github.com/cloudopsworks/terraform-module-aws-kms.git?ref=v1.0.1"
   is_hub     = var.is_hub
   org        = var.org
   spoke_def  = var.spoke_def
@@ -16,5 +16,36 @@ module "kms" {
     deletion_window_in_days = 30
     description             = "KMS key for AWS Prometheus Workspaces in ${var.org.organization_name} - ${var.org.environment_name}"
     enable_key_rotation     = try(var.kms.enable_key_rotation, false)
+    rotation_period_in_days = try(var.kms.rotation_period_in_days, 90)
+    statements = [
+      {
+        sid = "CloudWatchLogs"
+        actions = [
+          "kms:Encrypt*",
+          "kms:Decrypt*",
+          "kms:ReEncrypt*",
+          "kms:GenerateDataKey*",
+          "kms:Describe*"
+        ]
+        resources = ["*"]
+
+        principals = [
+          {
+            type        = "Service"
+            identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
+          }
+        ]
+
+        conditions = [
+          {
+            test     = "ArnLike"
+            variable = "kms:EncryptionContext:aws:logs:arn"
+            values = [
+              "arn:aws:logs:${local.region}:${data.aws_caller_identity.current.account_id}:log-group:*",
+            ]
+          }
+        ]
+      }
+    ]
   }
 }
